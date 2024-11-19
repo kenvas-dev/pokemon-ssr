44a6eaefdac5859564825c8f2cee14e6a8fa9556
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-11-19 17:49:43 -0600

    added about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'about-page',
@@ -10,4 +11,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent { }
+export default class AboutPageComponent implements OnInit {
+  private _title = inject(Title);
+  private _meta = inject(Meta);
+  
+  ngOnInit(): void {
+    this._title.setTitle('About page');
+    this._meta.updateTag({
+      name: 'description',
+      content: 'este es mi about page'
+    });
+    this._meta.updateTag({
+      name: 'og:title',
+      content: 'About page'
+    });
+    this._meta.updateTag({
+      name: 'keywords',
+      content: 'Hola, Mundo, Kevin, Aquino'
+    });
+  } 
+}
