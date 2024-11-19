65a9b6ee565b55acaa77b8e3713ac77a5bd5b187
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-11-19 17:50:02 -0600

    added pricing page

--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pricing-page',
@@ -10,4 +11,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit {
+  private _title = inject(Title);
+  private _meta = inject(Meta);
+  
+  ngOnInit(): void {
+    this._title.setTitle('Pricing page');
+    this._meta.updateTag({
+      name: 'description',
+      content: 'este es mi Pricing page'
+    });
+    this._meta.updateTag({
+      name: 'og:title',
+      content: 'Pricing page'
+    });
+    this._meta.updateTag({
+      name: 'keywords',
+      content: 'Hola, Mundo, Kevin, Aquino'
+    });
+  } 
+}
