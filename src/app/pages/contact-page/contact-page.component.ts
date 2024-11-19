4e01a2bfba5e6ca8827e621bbb12be647c2a8bcd
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-11-19 17:49:52 -0600

    added contact page

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'contact-page',
@@ -10,4 +11,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactPageComponent { }
+export default class ContactPageComponent implements OnInit {
+  private _title = inject(Title);
+  private _meta = inject(Meta);
+  
+  ngOnInit(): void {
+    this._title.setTitle('Contact page');
+    this._meta.updateTag({
+      name: 'description',
+      content: 'este es mi Contact page'
+    });
+    this._meta.updateTag({
+      name: 'og:title',
+      content: 'Contact page'
+    });
+    this._meta.updateTag({
+      name: 'keywords',
+      content: 'Hola, Mundo, Kevin, Aquino'
+    });
+  } 
+}
