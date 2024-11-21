1fe9a94f2f29d8d5d92849c53aba1ddfd0073c7b
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-11-20 22:30:12 -0600

    added new components ui

---
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "../../pokemons/components/pokemon-list-skeleton/pokemon-list-skeleton.component";
+
+@Component({
+  selector: 'pokemons-page',
+  standalone: true,
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  templateUrl: './pokemons-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PokemonsPageComponent implements OnInit {
+
+  public isLoading = signal(true);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 5000)
+  } 
+}
